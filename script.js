0f4d63714eede53b069ab37dcbde388d7b99ab75
lboku <129886726+lboku@users.noreply.github.com>
2024-07-20 11:57:47 +0400

    s

---
+++ script.js
@@ -0,0 +1,8 @@
+//დაამატეთ 2 რიცხვი და აჩვენეთ მიღებული შედეგი console.log -ის გამოყენებით
+console.log("2 + 2 =", 2 + 2);
+//გაამრავლეთ 2 რიცხვი და აჩვენეთ მიღებული შედეგი console.log -ის გამოყენებით
+console.log("3 * 3 =", 3 * 3);
+//დაწერეთ "Hello name" console.log -ის გამოყენებით (ჩაანაცვლეთ name -ი თქვენი სახელით!)
+console.log("hello lasha");
+//დაამატეთ 2 string -ი და აჩვენეთ მიღებული შედეგი console.log -ის გამოყენებით (string -ების დამატება) გამოიყენეთ მხოლოდ ასოები (a-z; A-Z)
+console.log("academy of " + "digital industries");
